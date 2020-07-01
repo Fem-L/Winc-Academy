846a009f0246964b85312729e7b60b5fd63cbd9a
Fem-L <62510411+Fem-L@users.noreply.github.com>
2020-07-01 12:23:08 +0200

    Add files via upload

---
+++ M03L01-01/script.js
@@ -0,0 +1,14 @@
+console.log('Hello Winc Academy');
+
+let name = 'Femke';
+console.log(name);
+
+let age = '42';
+console.log(age);
+
+console.log(3 + 3);
+console.log(5 - 3);
+console.log(3 - 9);
+console.log(3 * 9);
+console.log(8 / 2);
+console.log(15 % 2);
