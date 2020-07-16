531e95d82f601add5a94d3b6ce7889ab2b7a7da5
Fem-L <62510411+Fem-L@users.noreply.github.com>
2020-07-16 11:24:26 +0200

    Add files via upload

---
+++ M03L02-03_if-else-statements/index.js
@@ -0,0 +1,21 @@
+const age = 17;
+const isFemale = true;
+const driverStatus = 'piet';
+
+if (age > 17) {
+  console.log('Deze persoon is 18 jaar of ouder');
+} else {
+  console.log('Deze persoon is nog geen 18 jaar');
+}
+
+if (isFemale == true) {
+  console.log('Ze mag naar binnen op ladiesnight');
+} else {
+  console.log('Hij is niet welkom deze avond.');
+}
+
+if (driverStatus == 'bob') {
+  console.log('Deze persoon mag rijden');
+} else {
+  console.log('Deze persoon heeft gedronken en mag niet rijden');
+}
