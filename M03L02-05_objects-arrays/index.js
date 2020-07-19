62037a1f0045dfb9f4e05c83242fb9141c8d492e
Fem-L <62510411+Fem-L@users.noreply.github.com>
2020-07-19 12:28:29 +0200

    Add files via upload

---
+++ M03L02-05_objects-arrays/index.js
@@ -0,0 +1,139 @@
+//Deel 1: objecten
+
+let person = {
+  name: 'Femke',
+  age: 42,
+  evaluations: [7, 10, 9],
+};
+
+console.log(person);
+//Dot notation
+console.log('mijn naam is', person.name);
+console.log('mijn leeftijd is', person.age);
+// Bracket notation
+console.log('mijn naam is', person['name']);
+console.log('mijn leeftijd is', person['age']);
+
+console.log(person.evaluations); //de waarde is [7, 10, 9]
+
+//Deel 2: Arrays
+
+let listOfColors = ['groen', 'blauw', 'rood'];
+
+console.log(listOfColors);
+console.log('de lengte van de array is', listOfColors.length); //logt de lengte van de array
+console.log('het eerste element is', listOfColors[0]); //logt het eerste element van de array
+console.log('het laatste element is', listOfColors[listOfColors.length - 1]); //logt het laatste element
+
+listOfColors.push(5); //voegt een element toe aan een array
+console.log(listOfColors);
+
+listOfColors.push({ greeting: 'Hi, ik ben een object' }); //toevoegen van een object aan een array
+console.log(listOfColors);
+
+console.log('het laatste element is', listOfColors[listOfColors.length - 1]); //log het object dat het laatste element is
+
+//Deel 3
+//catBreeds is een array van objecten
+
+const catBreeds = [
+  {
+    name: 'Abyssinian',
+    description:
+      'The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.',
+    dog_friendly: 4,
+    energy_level: 5,
+    life_span: '14 - 15',
+    origin: 'Egypt',
+    temperament: [
+      'Active',
+      'Energetic',
+      'Independent',
+      'Intelligent',
+      'Gentle',
+    ],
+    wikipedia_url: 'https://en.wikipedia.org/wiki/Abyssinian_(cat)',
+    food: {
+      favourite_food: 'fish',
+      medium_liked_food: 'dried fruits',
+      disliked_food: 'walnuts',
+    },
+  },
+  {
+    name: 'Aegean',
+    description:
+      'Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.',
+    dog_friendly: 4,
+    energy_level: 53,
+    life_span: '9- 12',
+    origin: 'Greece',
+    temperament: ['Affectionate', 'Social', 'Intelligent', 'Playful', 'Active'],
+    wikipedia_url: 'https://en.wikipedia.org/wiki/Aegean_cat',
+    food: {
+      favourite_food: 'tuna',
+      medium_liked_food: 'canned food',
+      disliked_food: 'all fruits',
+    },
+  },
+  {
+    name: 'American Bobtail',
+    description:
+      'American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.',
+    dog_friendly: 5,
+    energy_level: 3,
+    life_span: '11 - 15',
+    origin: 'United States',
+    temperament: [
+      'Intelligent',
+      'Interactive',
+      'Lively',
+      'Playful',
+      'Sensitive',
+    ],
+    wikipedia_url: 'https://en.wikipedia.org/wiki/American_Bobtail',
+    food: {
+      favourite_food: 'meaty things',
+      medium_liked_food: 'tuna',
+      disliked_food: 'canned food',
+    },
+  },
+];
+
+//log de naam van het laatste kattenras
+
+let lastCat = catBreeds[catBreeds.length - 1];
+
+console.log('de naam van het laatste kattenras is', lastCat.name);
+
+//log het energielevel van de eerste kat
+
+console.log(
+  'het energie level van de eerste kat is',
+  catBreeds[0].energy_level
+);
+
+//Log het eerste temperament van de temperamenten van de tweede kat
+
+console.log(
+  'het eerste temperament van de tweede kat is',
+  catBreeds[1].temperament[0]
+);
+
+//Log het laatst temperament van de temperamenten van de derde kat
+
+/*Deze werkt niet zoals ik wil, ik krijg een nummer en geen naam
+
+let lastTemperament = lastCat.temperament.length - 1;
+console.log('het laatste temperament van de derde kat is', lastTemperament);*/
+
+console.log(
+  'het laatste temperament van de derde kat is',
+  catBreeds[2].temperament[4]
+);
+
+//Log het favoriete voedsel van de derde kat
+
+console.log(
+  'het favoriete eten van de derde kat is',
+  catBreeds[2].food.favourite_food
+);
