ff6fa8618aaadd8563c86365dd46100d0cb6c0d5
Femke Lemmens <62510411+Fem-L@users.noreply.github.com>
2020-09-28 17:43:30 +0200

    opdracht meeste mensen

--- M05L24_groteArraysObjecten/script.js
+++ M05L24_groteArraysObjecten/script.js
@@ -11,7 +11,7 @@ const removeList = () => {
   }
 };
 
-//SorteerFunctie
+//Algemene sorteerFunctie
 const sorteer = (resultaat) => {
   if (resultaat) {
     return 1;
@@ -24,7 +24,7 @@ const sorteer = (resultaat) => {
 //FilterFunctie
 let getCountries = randomPersonData
   .map((land) => land.region) //sorteert op de landen
-  .sort(); //zet lijst in alfabetische volgorde
+  .sort(); //in alfabetische volgorde
 let landen = new Set(getCountries); //maakt van een array een Set, verwijderd daarmee alle duplicaten
 getCountries = Array.from(landen); //zet Set om naar array
 
@@ -48,9 +48,9 @@ const addCountriesToDom = (array) => {
 //   return 0;
 // });
 
-const isFemale = (person) => person.gender === "female";
-const isOver30 = (person) => person.age > 30;
-const sortByName = (woman1, woman2) => sorteer(woman1.name > woman2.name);
+const isFemale = (person) => person.gender === "female"; //selecteer vrouwen
+const isOver30 = (person) => person.age > 30; //selecteer boven de 30 jaar
+const sortByName = (woman1, woman2) => sorteer(woman1.name > woman2.name); //sorteer op alfabet
 const isSteenbok = (person) => {
   let birthdays = person.birthday.mdy.split("/");
   if (
@@ -83,9 +83,9 @@ const addWomenToDom = (array) => {
 
 //3: ouwe creditcards
 
-//Sorteerfuncties
+//Sorteerfunctie
 
-const getAdults = (person) => person.age >= 18;
+const getAdults = (person) => person.age >= 18; //selecteert volwassenen
 
 //Functie om datum om te zetten naar losse getallen
 const convertExpDate = (card) => {
@@ -104,7 +104,7 @@ const oneYearFromNow = new Date();
 oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
 
 //Functie filtert op verloopdatum binnen een jaar
-const expDateWithinYear = (card) => {
+const getExpDateWithinYear = (card) => {
   let year = today.getFullYear().toString().split(""); //fulYear geeft 4 nummers
   let thisYear = year[2] + year[3]; //dit jaar teruggebracht tot twee nummers
   let yearAhead = oneYearFromNow.getFullYear().toString().split("");
@@ -119,13 +119,15 @@ const expDateWithinYear = (card) => {
 const sortByExpMonth = (card1, card2) =>
   card1.expDateCreditcard.expMonth - card2.expDateCreditcard.expMonth;
 
+//FilterFunctie
 const getPeople = randomPersonData
   .filter(getAdults)
   .map(convertExpDate)
-  .filter(expDateWithinYear)
+  .filter(getExpDateWithinYear)
   .sort(sortByExpMonth);
 //console.log(getPeople);
 
+//plaatsingfunctie
 const addPeopleToDom = (array) => {
   array.forEach((randomPersonData) => {
     const listItem = document.createElement("li"); //li element maken
@@ -144,9 +146,39 @@ const addPeopleToDom = (array) => {
 };
 
 //4: meeste mensen
-// Maak een lijst van alle landen die voorkomen in de data.
+
+//Functies;
+//Alle landen die voorkomen in de data.
+let countries = randomPersonData.map((country) => country.region);
+
+// optellen van de duplicaten in een array (stackoverflow)
+const countingPeople = countries.reduce((prev, cur) => {
+  prev[cur] = (prev[cur] || 0) + 1;
+  return prev;
+}, {});
+//console.log(countingPeople);
+
+//een array met objecten van de resultaten
+const getPeoplePerCoutry = [];
+for (land in countingPeople)
+  getPeoplePerCoutry.push({ land: land, aantal_mensen: countingPeople[land] });
+//console.log(getPeoplePerCoutry);
+
+//SorteerFunctie; De lijst moet zo gesorteerd zijn dat de landen met de meeste mensen bovenaan staan.
+const sortByNumberOfPeople = getPeoplePerCoutry.sort(
+  (a, b) => b.aantal_mensen - a.aantal_mensen
+);
+//console.log(sortByNumberOfPeople);
+
+//plaatsingsfunctie
 // Achter elk land moet komen te staan hoeveel van de mensen in de lijst in dat land wonen.
-// De lijst moet zo gesorteerd zijn dat de landen met de meeste mensen bovenaan staan.
+const addCountriesToDom2 = (array) => {
+  array.forEach((sortByNumberOfPeople) => {
+    const listItem = document.createElement("li"); //li element maken
+    dataList.appendChild(listItem);
+    listItem.innerHTML = `Land: ${sortByNumberOfPeople.land}  (${sortByNumberOfPeople.aantal_mensen})`;
+  });
+};
 
 //5: gemiddelde leeftijd
 // Als we op de knop voor deze opdracht drukken komt er een lijst met knoppen te staan. De opdracht-knoppen blijven ook staan.
@@ -172,12 +204,30 @@ const addPeopleToDom = (array) => {
 // -zien we de aangeklikte persoon bovenaan staan
 // -daaronder zien we een lijst van "matches" van die persoon
 // -iemand mag niet met zichzelf matchen
+// functie om data in de DOM te plaatsen:
+// const addDataToDom = (array) => {
+//   array.forEach((randomPersonData) => {
+//     const listItem = document.createElement("li"); //li element maken
+//     dataList.appendChild(listItem);
+//     const textItem = document.createElement("p");
+//     listItem.appendChild(textItem);
+//     if (array === "getCountries") {
+//       textItem.innerHTML = `${getCountries}`;
+//     } else if (array === "getSteenbokvrouwen") {
+//       textItem.innerHTML = `${randomPersonData.name} ${randomPersonData.surname} `; //laat voor- en achternaam en hun foto zien
+//       const portretImg = document.createElement("img"); // img element maken
+//       listItem.appendChild(portretImg); // portretImg toevoegen aan li element
+//       portretImg.setAttribute("class", "portret-img");
+//       portretImg.setAttribute("src", "https://picsum.photos/200");
+//     }
+//   });
+// };
 
 //switch functie om de verschillende buttons aan te sturen
 const addListHandler = (event) => {
-  const filterexpMonth = event.target.id;
-  console.log(filterexpMonth);
-  const expr = filterexpMonth;
+  const filterName = event.target.id;
+  console.log(filterName);
+  const expr = filterName;
   switch (expr) {
     case "landenlijst":
       removeList();
@@ -186,7 +236,7 @@ const addListHandler = (event) => {
     case "steenbokvrouwen":
       removeList();
       addWomenToDom(getSteenbokvrouwen);
-      console.log("steenbokvrouwen", getSteenbokvrouwen);
+      //console.log("steenbokvrouwen", getSteenbokvrouwen);
       break;
     case "ouwe-creditcards":
       removeList();
@@ -195,7 +245,7 @@ const addListHandler = (event) => {
       break;
     case "meeste-mensen":
       removeList();
-
+      addCountriesToDom2(sortByNumberOfPeople);
       break;
     case "gemiddelde-leeftijd":
       removeList();
